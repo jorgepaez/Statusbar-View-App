c2acc4d85c43c233a47bd1f081eeb147551e3f32
jorgepaez <jorgepaez@jorges-macbook-pro.local>
2016-04-20 21:43:32 -0700

    Copied and used the implementation of EventMonitor from https://www.raywenderlich.com/98178/os-x-tutorial-menus-popovers-menu-bar-apps

--- Statusbar-View-App/AppDelegate.swift
+++ Statusbar-View-App/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
     let popover = NSPopover()
+    var eventMonitor: EventMonitor?
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
@@ -23,6 +24,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
         
         popover.contentViewController = RootViewController(nibName: "RootViewController", bundle: nil)
+        
+        eventMonitor = EventMonitor(mask: [.LeftMouseDownMask, .RightMouseDownMask]) { [unowned self] event in
+            if self.popover.shown {
+                self.closePopover(event)
+            }
+        }
+        //eventMonitor?.start()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
@@ -32,11 +40,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func showPopover(sender: AnyObject?) {
         if let button = statusItem.button {
             popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
+            eventMonitor?.start()
         }
     }
     
     func closePopover(sender: AnyObject?) {
         popover.performClose(sender)
+        eventMonitor?.stop()
     }
     
     func togglePopover(sender: AnyObject?) {
